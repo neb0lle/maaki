cc75350bcdbfeab057ab815e888815c5e7939e8c
Aryanmallik <aryanmallik2004@gmail.com>
2024-03-23 19:12:28 +0530

    form

---
+++ src/app/model/[id]/create/token-classification/layout.jsx
@@ -0,0 +1,17 @@
+import { Theme } from "@radix-ui/themes";
+import "../../../../../styles/global.css";
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <title>MAKI</title>
+      </head>
+
+      <body>
+        <Theme appearance="dark">
+          <div className="container">{children}</div>
+        </Theme>
+      </body>
+    </html>
+  );
+}
