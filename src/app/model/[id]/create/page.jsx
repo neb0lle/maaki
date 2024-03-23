709d96830b9a30ef182705afb544b97f82083ec2
Aryanmallik <aryanmallik2004@gmail.com>
2024-03-24 04:39:15 +0530

    ui update

--- src/app/model/[id]/create/page.jsx
+++ src/app/model/[id]/create/page.jsx
@@ -1,121 +1,121 @@
 "use client";
 import "@radix-ui/themes/styles.css";
-import { Button } from "@radix-ui/themes";
+import { Button, Box, Card, Inset, Strong, Text } from "@radix-ui/themes";
 import { useState } from "react";
 import Link from "next/link";
 import styles from "@/styles/create.module.css";
 import Header from "../../../../../components/header";
 
 const Create = () => {
-  const [isExpanded, setIsExpanded] = useState("");
-  const [expandedButtons, setExpandedButtons] = useState([]);
-  const [selectedButtonIndex, setSelectedButtonIndex] = useState(null);
+    const [isExpanded, setIsExpanded] = useState("");
+    const [expandedButtons, setExpandedButtons] = useState([]);
+    const [selectedButtonIndex, setSelectedButtonIndex] = useState(null);
 
-  const buttonData = {
-    text: [
-      { label: "Sentiment Analysis", url: "./create/sa" },
-      { label: "Named Entity Recognition Tagging", url: "./create/nert" },
-      { label: "Natural Language Understanding", url: "./create/nlu" },
-    ],
-    image: [
-      {
-        label: "Single Label Classification",
-        url: "./create/slc",
-      },
-      {
-        label: "Multi Label Classification",
-        url: "./create/mlc",
-      },
-    ],
-    audio: [],
-    tabular: [],
-  };
+    const buttonData = {
+        text: [
+            { label: "Sentiment Analysis", url: "./create/sa" },
+            { label: "Named Entity Recognition Tagging", url: "./create/nert" },
+            { label: "Natural Language Understanding", url: "./create/nlu" },
+        ],
+        image: [
+            {
+                label: "Single Label Classification",
+                url: "./create/slc",
+            },
+            {
+                label: "Multi Label Classification",
+                url: "./create/mlc",
+            },
+        ],
+        audio: [],
+        tabular: [],
+    };
 
-  const toggleExpansion = (type) => {
-    if (isExpanded === type) {
-      setIsExpanded("");
-      setSelectedButtonIndex(null);
-    } else {
-      setIsExpanded(type);
-      setExpandedButtons(buttonData[type]);
-    }
-  };
-  const handleButtonClick = (type) => {
-    if (selectedButtonIndex === type) {
-      setSelectedButtonIndex(null);
-    } else {
-      setSelectedButtonIndex(type);
-    }
-  };
-  return (
-    <div className={styles.home}>
-      <Header />
-      <div className={styles.container}>
-        <div
-          className={`${styles.indexBox} ${isExpanded ? styles.expanded : ""}`}
-        >
-          <Button
-            size="4"
-            variant="soft"
-            onClick={() => {
-              toggleExpansion("text");
-            }}
-            className={isExpanded === "text" ? styles.selected : ""}
-          >
-            Text
-          </Button>
-          <Button
-            size="4"
-            variant="soft"
-            onClick={() => {
-              toggleExpansion("image");
-            }}
-            className={isExpanded === "image" ? styles.selected : ""}
-          >
-            Image
-          </Button>
-          <Button
-            size="4"
-            variant="soft"
-            onClick={() => {
-              toggleExpansion("audio");
-            }}
-            className={isExpanded === "audio" ? styles.selected : ""}
-          >
-            Audio
-          </Button>
-          <Button
-            size="4"
-            variant="soft"
-            onClick={() => {
-              toggleExpansion("tabular");
-            }}
-            className={isExpanded === "tabular" ? styles.selected : ""}
-          >
-            Tabular
-          </Button>
-        </div>
-        {isExpanded && (
-          <div className={styles.overlay}>
-            {expandedButtons.map((button, index) => (
-              <Link key={index} href={button.url}>
-                <Button
-                  size="4"
-                  variant="soft"
-                  onClick={() => handleButtonClick(button.label)}
-                  className={
-                    selectedButtonIndex === button.label ? styles.selected : ""
-                  }
+    const toggleExpansion = (type) => {
+        if (isExpanded === type) {
+            setIsExpanded("");
+            setSelectedButtonIndex(null);
+        } else {
+            setIsExpanded(type);
+            setExpandedButtons(buttonData[type]);
+        }
+    };
+    const handleButtonClick = (type) => {
+        if (selectedButtonIndex === type) {
+            setSelectedButtonIndex(null);
+        } else {
+            setSelectedButtonIndex(type);
+        }
+    };
+    return (
+        <div className={styles.home}>
+            <Header />
+            <div className={styles.container}>
+                <div
+                    className={`${styles.indexBox} ${isExpanded ? styles.expanded : ""}`}
                 >
-                  {button.label}
-                </Button>
-              </Link>
-            ))}
-          </div>
-        )}
-      </div>
-    </div>
-  );
+                    <Button
+                        size="4"
+                        variant="soft"
+                        onClick={() => {
+                            toggleExpansion("text");
+                        }}
+                        className={isExpanded === "text" ? styles.selected : ""}
+                    >
+                        Text
+                    </Button>
+                    <Button
+                        size="4"
+                        variant="soft"
+                        onClick={() => {
+                            toggleExpansion("image");
+                        }}
+                        className={isExpanded === "image" ? styles.selected : ""}
+                    >
+                        Image
+                    </Button>
+                    <Button
+                        size="4"
+                        variant="soft"
+                        onClick={() => {
+                            toggleExpansion("audio");
+                        }}
+                        className={isExpanded === "audio" ? styles.selected : ""}
+                    >
+                        Audio
+                    </Button>
+                    <Button
+                        size="4"
+                        variant="soft"
+                        onClick={() => {
+                            toggleExpansion("tabular");
+                        }}
+                        className={isExpanded === "tabular" ? styles.selected : ""}
+                    >
+                        Tabular
+                    </Button>
+                </div>
+                {isExpanded && (
+                    <div className={styles.overlay}>
+                        {expandedButtons.map((button, index) => (
+                            <Link key={index} href={button.url}>
+                                <Button
+                                    size="4"
+                                    variant="soft"
+                                    onClick={() => handleButtonClick(button.label)}
+                                    className={
+                                        selectedButtonIndex === button.label ? styles.selected : ""
+                                    }
+                                >
+                                    {button.label}
+                                </Button>
+                            </Link>
+                        ))}
+                    </div>
+                )}
+            </div>
+        </div>
+    );
 };
 
 export default Create;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,10 +6,10 @@ import {
   Button,
   Text,
   HoverCard,
-  Avatar,
-  Flex,
   Box,
-  Heading,
+    Card,
+    Inset,
+    Strong,
 } from "@radix-ui/themes";
 import Link from "next/link";
 import Header from "../../components/header";
@@ -20,25 +20,50 @@ const App = () => {
       <Header />
       <div className={styles.indexBox}>
         <div className={styles.homeBox}>
-          <Text>
-            <HoverCard.Root>
-              <HoverCard.Trigger>
-                <Button size="4" variant="soft">
-                  <Link className={styles.link} href={`/model/${id}/create`}>
-                    Create Model
-                  </Link>
-                </Button>
-              </HoverCard.Trigger>
-              <HoverCard.Content>
-                <Text size="2" style={{ maxWidth: 300 }} mt="3">
-                  Create your own model using our cwazy Ml
-                </Text>
-              </HoverCard.Content>
-            </HoverCard.Root>
-          </Text>
-          <Button size="4" variant="soft">
-            Use Model
-          </Button>
+      <Link className={styles.link} href={`/model/${id}/create`}>
+      <Box maxWidth="10px">
+  <Card size="1">
+    <Inset clip="padding-box" side="top" pb="current">
+      <img
+        src="/sushi1.png"
+        alt="Bold typography"
+        style={{
+          display: 'block',
+          height: 140,
+            width:'250px',
+          backgroundColor: 'var(--gray-5)',
+        }}
+      />
+    </Inset>
+    <Text as="p" size="3">
+      <Strong>Create Model</Strong><br/> 
+      Make your own Model using<br/>
+      Our 0 experience algorithim 
+    </Text>
+  </Card>
+</Box>
+      </Link>
+      <Box maxWidth="100px">
+  <Card size="1">
+    <Inset clip="padding-box" side="top" pb="current">
+      <img
+        src="/sushi2.png"
+        alt="Bold typography"
+        style={{
+          display: 'block',
+          height: 140,
+            width:'250px',
+          backgroundColor: 'var(--gray-5)',
+        }}
+      />
+    </Inset>
+    <Text as="p" size="3">
+      <Strong>Create Model</Strong><br/> 
+      Make your own Model using<br/>
+      Our 0 experience algorithim 
+    </Text>
+  </Card>
+</Box>
         </div>
       </div>
     </div>
